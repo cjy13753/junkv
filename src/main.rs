cf9fdcdeec38a8317c9867a65935ced5206718fb
Junyoung Choi(최준영) <cjy13753@gmail.com>
2023-08-14 20:05:44 +0900

    Implement simplest form of write opeartion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    fs::{self, OpenOptions},
+    io::Write,
+    path::Path,
+};
+
 fn main() {
-    println!("Hello, world!");
+    write("Name", "123456")
+}
+
+fn write(key: &str, value: &str) {
+    let path = Path::new("data/data.txt");
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+    let file_size = fs::metadata(path).unwrap().len();
+    let mut pair = format!("{key}:{value}");
+
+    if file_size != 0 {
+        pair = format!(",{pair}");
+    }
+    file.write_all(pair.as_bytes()).unwrap();
 }
